cfc3eeecc884c30d1f74e0cc57b61aa81d12a4ab
NikAlex <alez90@yandex.ru>
2016-12-17 13:16:05 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,35 +1,41 @@
-#include <sort.cpp>
+#include "sort_test.cpp"
 #include <catch.hpp>
-#include <string>
 #include <iostream>
+#include <string>
 #include <fstream>
+#include <chrono>
 using namespace std;
-
-SCENARIO("32mb", "[32mb]")
-{
-	std::chrono::time_point<std::chrono::system_clock> start, end;
+ 
+SCENARIO("32mb", "[32mb]"){
+ bool p=true;
+std::chrono::time_point<std::chrono::system_clock> start, end;
 	start = std::chrono::system_clock::now();
-	sort_it("32mb", "out_32", 17);
-	end = std::chrono::system_clock::now();
-	cout <<"32MB- " <<(end - start).count() <<" nanoseconds"<< endl;
-  REQUIRE(1);
-}
-SCENARIO("15mb", "[15mb]")
-{
-	std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
-	sort_it("15mb", "out_15", 4);
-	end = std::chrono::system_clock::now();
-	cout <<"15MB- " <<(end - start).count() <<" nanoseconds"<< endl;
-  REQUIRE(1);
+ B obj("32","out",17);
+ end = std::chrono::system_clock::now();
+	cout <<"32MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
+ 
+  REQUIRE(p==true);
 }
 
-SCENARIO("8mb", "[8mb]")
-{
-	std::chrono::time_point<std::chrono::system_clock> start, end;
+SCENARIO("15mb", "[15mb]"){
+ bool p=true;
+std::chrono::time_point<std::chrono::system_clock> start, end;
 	start = std::chrono::system_clock::now();
-	sort_it("8mb", "out_8", 1);
-	end = std::chrono::system_clock::now();
-	cout <<"8MB- " <<(end - start).count() <<" nanoseconds"<< endl;
-  REQUIRE(1);
+B obj("15","out2",4);
+ end = std::chrono::system_clock::now();
+	cout <<"15MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
+ 
+  REQUIRE(p==true);
 }
+
+SCENARIO("8mb", "[8mb]"){
+bool p=true;
+std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+  B obj("8","out8",1);
+ end = std::chrono::system_clock::now();
+	cout <<"8MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
+ 
+  REQUIRE(p==true);
+}
+
