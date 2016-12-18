5b11127a5b686e49c376192758f157f891a65408
NikAlex <alez90@yandex.ru>
2016-12-18 11:24:06 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,41 +1,75 @@
-#include "sort_1.cpp"
+#include <sort.cpp>
 #include <catch.hpp>
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <fstream>
-#include <chrono>
 using namespace std;
  
-SCENARIO("32mb", "[32mb]"){
- bool p=true;
-std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
- B obj("32","out",17);
- end = std::chrono::system_clock::now();
-	cout <<"32MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
- 
-  REQUIRE(p==true);
+SCENARIO("8mb", "[8mb]")
+{
+ sorting ("8mb", "out_8", 1);
+  bool x = true;
+  std::ifstream f1("out_8"), f2("out8");
+  man m1, m2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>m1)&&(f2>>m2))
+    {
+      if(m1.name!=m2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
 }
 
-SCENARIO("15mb", "[15mb]"){
- bool p=true;
-std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
-B obj("15","out2",4);
- end = std::chrono::system_clock::now();
-	cout <<"15MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
- 
-  REQUIRE(p==true);
+SCENARIO("15mb", "[15mb]")
+{
+  sorting ("15mb", "out_15", 4);
+  bool x = true;
+  std::ifstream f1("out_15"), f2("out15");
+  man m1, m2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>m1)&&(f2>>m2))
+    {
+      if(m1.name!=m2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
 }
 
-SCENARIO("8mb", "[8mb]"){
-bool p=true;
-std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
-  B obj("8","out8",1);
- end = std::chrono::system_clock::now();
-	cout <<"8MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
- 
-  REQUIRE(p==true);
+SCENARIO("32mb", "[32mb]")
+{
+  sorting ("32mb", "out_32", 17);
+  bool x = true;
+  std::ifstream f1("out_32"), f2("out32");
+  man m1, m2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>m1)&&(f2>>m2))
+    {
+      if(m1.name!=m2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
 }
-
